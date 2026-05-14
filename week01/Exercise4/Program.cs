cea082c793893db1dd9993af2822fa2c453a86b5
agravaineladdaran <agravaine.laddaran@gmail.com>
2026-05-15 02:49:54 +0800

    Add number list program with sum, average, and max calculation

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+
+        int user_input = -1;
+
+        while (user_input != 0)
+        {
+            Console.Write("Enter number: ");
+            user_input = int.Parse(Console.ReadLine());
+
+            if (user_input != 0)
+            {
+                numbers.Add(user_input);
+            }
+        }
+
+        if (numbers.Count > 0)
+        {
+
+            int sum = 0;
+            int max = numbers[0];
+
+            foreach (int num in numbers)
+            {
+                sum+=num;
+
+                if (num > max)
+                {
+                    max = num;
+                }
+            }
+
+            double average = (double)sum / numbers.Count;
+
+            Console.WriteLine($"The sum is: {sum} ");
+            Console.WriteLine($"The average is: {average} ");
+            Console.WriteLine($"The largest number is: {max}");
+        }
     }
 }
