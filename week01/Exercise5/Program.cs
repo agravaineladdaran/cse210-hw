54c368b116f711904de051dc4f5cf60197fe68b7
agravaineladdaran <agravaine.laddaran@gmail.com>
2026-05-15 06:21:12 +0800

    Add functions for welcome message, user input, and square calculation

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+
+        string name = PromptUserName();
+
+        int fav_num = PromptUserNumber();
+
+        int squaredNumber = SquareNumber(fav_num);
+
+        DisplayResult(name, squaredNumber);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int fav_num = int.Parse(Console.ReadLine());
+
+        return fav_num;
+    }
+
+    static int SquareNumber(int fav_num)
+    {
+        int square = fav_num * fav_num;
+
+        return square;
+    }
+
+    static void DisplayResult(string name, int squaredNumber)
+    {
+        Console.WriteLine($"{name}, the square of your favorite number is {squaredNumber}");
     }
 }
