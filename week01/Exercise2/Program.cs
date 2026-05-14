8ba6e18a92f71eab7b6aced6ab54431e01a9416e
agravaineladdaran <agravaine.laddaran@gmail.com>
2026-05-15 01:37:08 +0800

    Add grade calculator with letter grading and pass/fail check

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int grade_num = int.Parse(grade);
+
+        string letter;
+
+        if (grade_num>=90)
+        {
+            letter = "A";
+        }
+        else if (grade_num>=80)
+        {
+            letter = "B";
+        }
+        else if (grade_num>=70)
+        {
+            letter = "C";
+        }
+        else if (grade_num>=60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (grade_num>=70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass this time, but progress comes with effort. Keep trying!");
+        }
     }
 }
