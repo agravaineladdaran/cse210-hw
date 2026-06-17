aaf69ad8108f2c1aee086d31820f807723404792
agravaineladdaran <agravaine.laddaran@gmail.com>
2026-06-17 11:34:56 +0800

    Added comment on feature added that exeeded requirements

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,3 +1,12 @@
+// EXCEEDING REQUIREMENTS:
+// Added a level system that rewards users with titles
+// based on their accumulated score.
+// 0 - 499 points     = Beginner
+// 500 - 999 points = Disciple
+// 1000 - 1499 points = Pathfinder
+// 1500+ points     = Eternal Quest Master
+
+
 class Program
 {
     static void Main(string[] args)
